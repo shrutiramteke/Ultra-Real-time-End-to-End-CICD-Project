1d30f8c5120c692ea6e04f48fdb61ff0b7b92112
shrutiramteke <57669988+shrutiramteke@users.noreply.github.com>
2023-03-10 12:51:08 +0530

    Update DemoApplicationTests.java

--- src/test/java/project/demo/DemoApplicationTests.java
+++ src/test/java/project/demo/DemoApplicationTests.java
@@ -12,14 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplicationTests {
 	@GetMapping
 	public String message() {
-<<<<<<< HEAD
 
-=======
->>>>>>> 4541ad16ac649f142797606e4d26138e72fa048b;
 		return "welcome to our project";
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplicationTests.class, args)	;	
+		SpringApplication.run(DemoApplicationTests.class, args);	
 	}
 }
